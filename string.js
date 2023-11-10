bb7228f12bcf8289d862f83addb98894391d698e
Vinayaksolanke <122889031+Vinayaksolanke@users.noreply.github.com>
2023-11-10 19:52:08 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,28 @@
+let MyIntro = `i-am lerning javascipt`
+let MyAge = 22
+// here we concat two strings 
+console.log(`my name is vinayak ${MyIntro} and my age is: ${MyAge}.`)
+
+
+//STRING METHORDS
+console.log(MyIntro[2])// SHOWS 2ND NUMBER CHARACTRE IN THE STRING bacically string is a abject
+// 2 CONVERTING STRING IN THEUPPER CASE
+let uppercase=MyIntro.toUpperCase()
+console.log(uppercase)
+
+// LENGHT OF STRING
+//IMP ==>>....this is new way to define and print string methord
+console.log(`${MyIntro.toUpperCase()}`)
+
+console.log(`${MyIntro.replace("javascipt", "java")}`)
+
+console.log(`${MyIntro.search("java")}`)
+
+console.log(`${MyIntro.slice(4,12)}`)
+
+console.log(`${MyIntro.split('-')}`)
+
+
+
+
+
